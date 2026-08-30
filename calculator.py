f39be485c0cc5bec6335e9bc1f43604eb3c0750f
Nusumaru06 <nusumaru0624@gmail.com>
2026-08-31 02:51:40 +0900

    Day2: Create calculator app

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 #電卓アプリ
+#計算方法をユーザーが選択
 print("=== 電卓アプリ ===")
 print("1 : 足し算")
 print("2 : 引き算")
@@ -7,29 +8,31 @@ print("4 : 割り算")
 
 choice = input("選んでください: ")
 
-data1 = float(input("1つ目の数字:  "))
-data2 = float(input("2つ目の数字:  "))
+#数字を入力
+num1 = float(input("1つ目の数字:  "))
+num2 = float(input("2つ目の数字:  "))
 
+#計算処理と結果の表示
 result = None
 if choice == "1":
-    result = data1 + data2
-    print(f"結果:{data1} + {data2} = {result}")
+    result = num1 + num2
+    print(f"結果:{num1} + {num2} = {result}")
 
 elif choice == "2":
-    result = data1 - data2
-    print(f"結果:{data1} - {data2} = {result}")
+    result = num1 - num2
+    print(f"結果:{num1} - {num2} = {result}")
 
 elif choice == "3":
-    result = data1 * data2
-    print(f"結果:{data1} * {data2} = {result}")
+    result = num1 * num2
+    print(f"結果:{num1} * {num2} = {result}")
 
 elif choice == "4":
 
-    if data2 == 0:
+    if num2 == 0:
         print("0で割ることはできません")
     else:
-        result = data1 / data2
-        print(f"結果:{data1} / {data2} = {result}")
+        result = num1 / num2
+        print(f"結果:{num1} / {num2} = {result}")
 
 else:
     print("無効な選択です")

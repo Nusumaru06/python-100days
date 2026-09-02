c7dae7048bcaf9dff4a93d027e6a55a757d3ad77
Nusumaru06 <nusumaru0624@gmail.com>
2026-09-02 22:09:43 +0900

    Day5: random_string_game

--- day5_random_string.py
+++ day5_random_string.py
@@ -1,7 +1,50 @@
-#ランダムな文字を10文字出力
+#Random String Generator
 import random
 import string
 
-for i in range(10):
-    char = random.choice(string.ascii_lowercase)
-    print(char, end="")
+#生成する文字数の決定
+print("=== Random String Generator ===")
+length = int(input("生成する文字数を入力してください: "))
+
+#大文字を含めるかの選択
+def choice_1():
+    while True:
+        choice1 = input("大文字を含めますか？ (y/n): ")
+        if choice1 == "y":
+            return 1
+        elif choice1 == "n":
+            return 2
+        else:
+            print("無効な入力です。")
+upper_choice = choice_1()
+
+#数字を含めるかの選択
+def choice_2():
+    while True:
+        choice2 = input("数字を含めますか？ (y/n): ")
+        if choice2 == "y":
+            return 1
+        elif choice2 == "n":
+            return 2
+        else:
+            print("無効な入力です。")
+digit_choice = choice_2()
+
+#文字プールの作成
+if upper_choice == 1 and digit_choice == 1:
+    pool = string.ascii_lowercase + string.ascii_uppercase + string.digits
+
+elif upper_choice == 1 and digit_choice == 2:
+    pool = string.ascii_lowercase + string.ascii_uppercase
+
+elif upper_choice == 2 and digit_choice == 1:
+    pool = string.ascii_lowercase + string.digits
+
+else:
+    pool = string.ascii_lowercase
+
+#文字列の生成
+result = "".join(random.choices(pool, k = length))
+
+#結果の表示
+print(f"生成されたランダムな文字列は{length}文字で{result}です。")
